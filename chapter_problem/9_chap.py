2ef1831422b5155c05c1aa541b65fd435f5fdfe7
sg92468 <sg92468@gmail.com>
2021-09-23 00:39:50 +0900

    [Finished]9.9

--- chapter_problem/9_chap.py
+++ chapter_problem/9_chap.py
@@ -12,3 +12,27 @@ def nonbuggy(arg, result=None):
 nonbuggy('a')
 nonbuggy('b')
 nonbuggy('a', ['b', 'c'])
+
+def document_it(func):
+    def new_function(*args, **kwargs):
+        print('Running function:', func.__name__)
+        print('Positional arguments:', args)
+        print('Keyword arguments:', kwargs)
+        result = func(*args, **kwargs)
+        print('Result:', result)
+        return result
+    return new_function
+
+def square_it(func):
+    def new_function(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result * result
+    return new_function
+
+@square_it
+@document_it
+def add_ints(a, b):
+    return a + b
+
+print(add_ints)
+print(add_ints(3,5))
