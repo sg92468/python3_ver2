2ce5d4772b5c39b1a688edbf4fbcd5e28f00171f
sg92468 <sg92468@gmail.com>
2021-10-12 21:01:48 +0900

    [Finish] 15.3.2

--- chapter_problem/15_chap/15_chap.py
+++ chapter_problem/15_chap/15_chap.py
@@ -20,4 +20,21 @@ print(ret)
 ret = subprocess.call('date -u', shell=True)
 
 # 15.1.2 multprocessingのプロセス作成
+# mp.pyに記載
+
+# 15.1.3 terminate()によるプロセスの強制終了
+# mp2.pyに記載
+
+# 15.1.4 osでのシステム情報の入手
+import os
+print(os.uname())
+print(os.getloadavg())
+print(os.cpu_count())
+
+# 15.1.5 psutilによるプロセス情報の取得
+# import psutil
+# print(psutil.cpu_times(True))
+# print(psutil.cpu_percent(True))
+# print(psutil.cpu_percent(percpu=True))
+
 
---
+++ chapter_problem/15_chap/tasks.py
@@ -0,0 +1,11 @@
+import time
+
+from invoke import task
+
+@task
+def mytime(ctx):
+    now = time.time()
+    time_str = time.asctime(time.localtime(now))
+    print("Local time is", time_str)
+
+# 実装できていない。わからん
