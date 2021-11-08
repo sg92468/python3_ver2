985227dbab0b19648b5cfb65b51205b8d00d79da
sg92468 <sg92468@gmail.com>
2021-11-08 19:19:41 +0900

    [Finish] 18.1

--- chapter_problem/18_chap/18_chap.py
+++ chapter_problem/18_chap/18_chap.py
@@ -1,2 +1,22 @@
-# 18.1.1 telnet
+# 18.1.5 標準ウェブライブラリ
+import urllib.request
+url = 'http://www.example.com/'
+conn = urllib.request.urlopen(url)
+
+print(conn.status)
+
+data = conn.read()
+print(data[:50])
+
+str_data = data.decode('utf8')
+print(str_data)
+for key, value in conn.getheaders():
+    print(key, value)
+
+# 18.1.6 requests
+import requests
+resp = requests.get('http://example.com')
+print(resp)
+print(resp.status_code)
+print(resp.text[:50])
 
