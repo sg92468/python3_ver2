2a1ea8eaddc5e77fca3898940457096dd5a331a5
sg92468 <sg92468@gmail.com>
2021-11-11 20:32:01 +0900

    [Finish] chap18

--- chapter_problem/18_chap/18_chap.py
+++ chapter_problem/18_chap/18_chap.py
@@ -1,24 +1,25 @@
-# 18.1.5 標準ウェブライブラリ
-import urllib.request
-url = 'http://www.example.com/'
-conn = urllib.request.urlopen(url)
+# # 18.1.5 標準ウェブライブラリ
+# from logging import debug
+# import urllib.request
+# url = 'http://www.example.com/'
+# conn = urllib.request.urlopen(url)
 
-print(conn.status)
+# print(conn.status)
 
-data = conn.read()
-print(data[:50])
+# data = conn.read()
+# print(data[:50])
 
-str_data = data.decode('utf8')
-print(str_data)
-for key, value in conn.getheaders():
-    print(key, value)
+# str_data = data.decode('utf8')
+# print(str_data)
+# for key, value in conn.getheaders():
+#     print(key, value)
 
-# 18.1.6 requests
-import requests
-resp = requests.get('http://example.com')
-print(resp)
-print(resp.status_code)
-print(resp.text[:50])
+# # 18.1.6 requests
+# import requests
+# resp = requests.get('http://example.com')
+# print(resp)
+# print(resp.status_code)
+# print(resp.text[:50])
 
 # # 18.5.1 webbrowser
 # import antigravity
@@ -36,4 +37,38 @@ print(resp.text[:50])
 # pip3 install pywebview[qt]でインストールできず。
 # pip3 install pywebviewでインストールし上記コマンドをテストしたがwebviewは機能せず。
 
+# # q18.2
+# from flask import Flask
 
+# app = Flask(__name__)
+
+# app.run(port=5000, debug=True)
+
+# # q18.3
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "It's alive"
+
+# app.run(port=5000, debug=True)
+
+# # q18.4
+# from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     thing = request.values.get('thing')
+#     height = request.values.get('height')
+#     color = request.values.get('color')
+#     return render_template('home.html', thing=thing, height=height, color=color)
+# # クエリを使用しない場合
+# # @app.route('/<thing>/<height>/<color>/')
+# # def home(thing, height, color):
+# #     return render_template('home.html', thing=thing, height=height, color=color)
+
+# app.run(port=5000, debug=True)
