ac641e5c7079576710d6c7218623b8b4b80d940d
sg92468 <sg92468@gmail.com>
2021-11-13 21:26:36 +0900

    [Finish] 19.7.1

---
+++ chapter_problem/19_chap/style3.py
@@ -0,0 +1,12 @@
+"""ここにモジュールのdocstringを書く"""
+
+def func():
+    """ここに関数のdocstringを書く"""
+    first = 1
+    second = 2
+    third = 3
+    print(first)
+    print(second)
+    print(third)
+
+func()
