e47578bfcf0531fda657249a79e059d576ed9d54
sg92468 <sg92468@gmail.com>
2021-11-19 18:07:30 +0900

    [Finish] Chap 19

--- chapter_problem/19_chap/19_chap.py
+++ chapter_problem/19_chap/19_chap.py
@@ -37,3 +37,27 @@
 #     snooze()
 
 
+# # 19.10.5 Numba
+# import math
+# from timeit import timeit
+# from numba import jit
+
+# def hypot(a, b):
+#     return math.sqrt(a ** 2 + b ** 2)
+
+# print(timeit('hypot(5, 6)', globals=globals()))
+# print(timeit('hypot(5, 6)', globals=globals()))
+
+# @jit
+# def hypot_jit(a, b):
+#     return math.sqrt(a ** 2 + b ** 2)
+
+# print(timeit('hypot_jit(5, 6)', globals=globals()))
+# print(timeit('hypot_jit(5, 6)', globals=globals()))
+
+# @jit(nopython=True)
+# def hypot_jit_nopy(a, b):
+#     return math.sqrt(a ** 2 + b ** 2)
+
+# print(timeit('hypot_jit_nopy(5, 6)', globals=globals()))
+# print(timeit('hypot_jit_nopy(5, 6)', globals=globals()))
